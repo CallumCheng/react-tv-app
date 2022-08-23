454c9132cb8d87f1f589dcb409108f67ac4fed31
CallumCheng <callumcheng@outlook.com>
2022-08-23 17:01:01 +0100

    changed function to pass testing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = () => {
     return <header>
          <h1>Search your favourite shows here</h1>
-         <em>Sub-Heading</em>
     </header>
 }
 
--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 const ShowCard = ({data}) =>{
 
     return <div className="show-card">
-            {/* {data["image"] && <img src={data["image"]["medium"]}></img>} As a React function */}
-            <img src={ data["image"] ? data["image"]["medium"] : "" }></img>  
+            {data["image"] && <img src={data["image"]["medium"]}></img>} 
+            {/* <img src={ data["image"] ? data["image"]["medium"] : "" }></img>   */}
             <h2>{data.name}</h2>
             <p role="paragraph">{data.summary}</p>
             </div>
